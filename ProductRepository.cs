1fb97aedc58621f3b51f28dfaa890646cbe99c0d
Joseph Broome <josephlloydbroome@gmail.com>
2022-08-02 07:00:07 -0700

    updates

---
+++ ProductRepository.cs
@@ -0,0 +1,65 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_DEMO.NET_
+{
+    internal class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _connection.Execute("INSERT INTO products (name, price, categoryID) VALUES (@pname, @pprice, @pcategoryID);",
+                new { pname = name, pprice = price, pcategoryID = categoryID }); 
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM products ORDER BY  name   ");
+        }
+
+
+        public ProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+
+        public void UpdateProductName(int productID, string newname)
+        {
+            _connection.Execute("UPDATE products SET Name = @newName WHERE ProductID = @productID;",
+                new { newName =newname, productID = productID });
+        }
+
+        public void DeleteProduct(int productID)
+        {
+
+           
+            _connection.Execute("DELETE FROM products WHERE ProductID = @productID ;",
+                 new { productID = productID });
+
+            _connection.Execute("DELETE FROM sales WHERE ProductID = @productID ;",
+                new { productID = productID });
+
+            _connection.Execute("DELETE FROM reviews WHERE ProductID = @productID ;",
+                new { productID = productID });
+
+            Console.WriteLine("Product has been succsefully deleted!");
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,105 @@
-﻿
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using Dapper_DEMO.NET_;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var repo = new DepartmentRepository(conn);
+
+var productrepo = new ProductRepository(conn);
+
+repo.InsertDepartment("My new Department!");
+
+var departments = repo.GetAllDepartments();
+
+foreach (var department in departments)
+{
+    Console.WriteLine(department.Name);
+    Console.WriteLine(department.DepartmentID);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+var products = productrepo.GetAllProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.Name);
+    Console.WriteLine($"${product.Price}");
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+bool isnumber;
+bool isnubmer2;
+do
+{
+    Console.WriteLine("Enter a new product name to add the the list of products");
+    var newproductname = Console.ReadLine();
+    Console.WriteLine();
+    Console.WriteLine("Enter the price of the new product");
+    var userinput = Console.ReadLine();
+    double newproductprice;
+    isnumber = double.TryParse(userinput, out newproductprice);
+    Console.WriteLine();
+    Console.WriteLine("Enter the CategoryID for the new product");
+    var userinput2 = Console.ReadLine();
+    int productcategoryid;
+    isnubmer2 = int.TryParse(userinput2, out productcategoryid);
+
+    productrepo.CreateProduct(newproductname, newproductprice, productcategoryid);
+
+} while (isnumber == false || isnubmer2 == false);
+
+var newproducts =productrepo.GetAllProducts();
+
+foreach (var product in newproducts)
+{
+    Console.WriteLine("************************************");
+    Console.WriteLine(product.Name);
+    Console.WriteLine($"${product.Price}");
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine();
+    Console.WriteLine();
+
+}
+
+productrepo.UpdateProductName(940, "Halo Infinite 2");
+
+var updatedproducts = productrepo.GetAllProducts();
+
+foreach(var product in updatedproducts)
+{
+    Console.WriteLine("************************************");
+    Console.WriteLine(product.Name);
+    Console.WriteLine($"${product.Price}");
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+productrepo.DeleteProduct(940);
+
+var deletedproducts = productrepo.GetAllProducts();
+
+foreach( var product in deletedproducts)
+{
+    Console.WriteLine("************************************");
+    Console.WriteLine(product.Name);
+    Console.WriteLine($"${product.Price}");
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+    
+
+
